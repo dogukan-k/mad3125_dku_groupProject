9e34f893ec2ba2f8a1d46a2a9e41428551de7013
dogukan-k <karayilanoglu35@hotmail.com>
2019-12-14 10:19:21 -0500

    MainActivity Layout filled

--- app/src/main/java/com/dku/group_dku_755495_fp/Controller/MainActivity.java
+++ app/src/main/java/com/dku/group_dku_755495_fp/Controller/MainActivity.java
@@ -3,14 +3,26 @@ package com.dku.group_dku_755495_fp.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.dku.group_dku_755495_fp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText searchBar;
+    ListView listView;
+    Button addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBar = findViewById(R.id.searchText);
+        listView = findViewById(R.id.listView);
+        addButton = findViewById(R.id.add_button);
+
     }
 }
---
+++ app/src/main/java/com/dku/group_dku_755495_fp/Controller/RegisterFormActivity.java
@@ -0,0 +1,16 @@
+package com.dku.group_dku_755495_fp.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dku.group_dku_755495_fp.R;
+
+public class RegisterFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+    }
+}
