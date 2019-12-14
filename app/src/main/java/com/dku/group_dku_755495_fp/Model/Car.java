d7bc4f07389c92710748f00429861acf7f45813b
dogukan-k <karayilanoglu35@hotmail.com>
2019-12-14 09:13:20 -0500

    Car subclass created

---
+++ app/src/main/java/com/dku/group_dku_755495_fp/Model/Car.java
@@ -0,0 +1,25 @@
+package com.dku.group_dku_755495_fp.Model;
+
+public class Car extends  Vehicle {
+
+    private String type ;
+
+
+    public Car(String make, String plate, String color, String category, String type) {
+        super(make, plate, color, category);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toDisplay() {
+        return super.toDisplay();
+    }
+}
