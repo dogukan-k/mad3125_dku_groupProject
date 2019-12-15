01627d773f4e21569e456593cc5d66d15ffc515a
dogukan-k <karayilanoglu35@hotmail.com>
2019-12-15 16:14:30 -0500

    employeedetailactivity layout binded

--- app/src/main/java/com/dku/group_dku_755495_fp/Controller/EmployeeDetailActivity.java
+++ app/src/main/java/com/dku/group_dku_755495_fp/Controller/EmployeeDetailActivity.java
@@ -1,14 +1,23 @@
-package com.dku.group_dku_755495_fp;
+package com.dku.group_dku_755495_fp.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.dku.group_dku_755495_fp.R;
 
 public class EmployeeDetailActivity extends AppCompatActivity {
+
+
+    TextView detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_detail);
+
+        detail = findViewById(R.id.txt_detail);
     }
 }
