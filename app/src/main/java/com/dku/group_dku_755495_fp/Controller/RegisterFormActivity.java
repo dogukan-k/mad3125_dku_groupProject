8aeebd5a2d1e2634efe4ba0b30809a6477679a8d
dogukan-k <karayilanoglu35@hotmail.com>
2019-12-15 16:05:21 -0500

    registeractivity carradiobutton updated

--- app/src/main/java/com/dku/group_dku_755495_fp/Controller/RegisterFormActivity.java
+++ app/src/main/java/com/dku/group_dku_755495_fp/Controller/RegisterFormActivity.java
@@ -344,6 +344,7 @@ public class RegisterFormActivity extends AppCompatActivity {
                                 vehicle.setPlate(plateNumber.getText().toString());
                                 vehicle.setColor(vehicleColor.getSelectedItem().toString());
                                 ((Car)vehicle).setType(carType.getText().toString());
+                                vehicle.setCategory("");
                             }
 
 
