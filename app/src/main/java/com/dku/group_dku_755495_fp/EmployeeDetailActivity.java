40d8e60281747a4b784a947e9e4f4f542cf59e22
dogukan-k <karayilanoglu35@hotmail.com>
2019-12-15 16:11:20 -0500

    employeedetailactivity added

---
+++ app/src/main/java/com/dku/group_dku_755495_fp/EmployeeDetailActivity.java
@@ -0,0 +1,14 @@
+package com.dku.group_dku_755495_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_detail);
+    }
+}
